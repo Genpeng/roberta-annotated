381c9c365563f0ef6a502b9191782a7947154373
Genpeng Xu <xgp1227@gmail.com>
2022-09-23 17:38:46 +0800

    :memo: modify run_ner_msra.sh

--- run_ner_msra.sh
+++ run_ner_msra.sh
@@ -5,26 +5,27 @@ set -uex
 CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 TASK_NAME="msra_ner"
-DATA_DIR="$CURRENT_DIR/data"
-MODEL_DIR="/Users/gerry.xu/pretrained_model/chinese_roberta_wwm_ext_L-12_H-768_A-12"
-OUTPUT_DIR="$CURRENT_DIR/output/$TASK_NAME"
+DATA_DIR="$CURRENT_DIR/datasets"
+MODEL_NAME="chinese_roberta_wwm_ext_L-12_H-768_A-12"
+MODEL_DIR="$CURRENT_DIR/models/$MODEL_NAME"
+OUTPUT_DIR="$CURRENT_DIR/outputs/$TASK_NAME"
 
 if [ ! -d $OUTPUT_DIR ]; then
   mkdir -p $OUTPUT_DIR
   echo "makedir $OUTPUT_DIR"
 fi
 
-#python run_ner.py \
-#  --task_name=$TASK_NAME \
-#  --do_train=true \
-#  --do_eval=false \
-#  --do_predict=true \
-#  --data_dir=$DATA_DIR/$TASK_NAME \
-#  --vocab_file=$MODEL_DIR/vocab.txt \
-#  --bert_config_file=$MODEL_DIR/bert_config.json \
-#  --init_checkpoint=$MODEL_DIR/bert_model.ckpt \
-#  --max_seq_length=256 \
-#  --train_batch_size=16 \
-#  --learning_rate=2e-5 \
-#  --num_train_epochs=5.0 \
-#  --output_dir=$OUTPUT_DIR \
+python run_ner.py \
+  --task_name=$TASK_NAME \
+  --do_train=true \
+  --do_eval=false \
+  --do_predict=true \
+  --data_dir=$DATA_DIR/$TASK_NAME \
+  --vocab_file=$MODEL_DIR/vocab.txt \
+  --bert_config_file=$MODEL_DIR/bert_config.json \
+  --init_checkpoint=$MODEL_DIR/bert_model.ckpt \
+  --max_seq_length=256 \
+  --train_batch_size=16 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=5.0 \
+  --output_dir=$OUTPUT_DIR \
